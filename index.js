d7f6d1e1c126ed93a18b61fb31ede96cab325905
himanshu mandwal <hkushwah8@gmail.com>
2020-09-17 12:40:03 +0530

    created directory structure and setup express server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const port=8000;
+
+
+
+
+
+
+
+const app=express();
+
+
+
+
+
+
+app.listen(port,function(err){
+    if(err)
+    {
+        console.log(`Error in setting up the server ${err}`);
+    }
+    else
+    {
+        console.log(`server is up and running on the port ${port}`);
+    }
+})
